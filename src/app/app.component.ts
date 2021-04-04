c20c642e0234adb457777e1752a7b44ea9da5a79
Maura Joglekar <m.joglekar@f5.com>
2021-04-04 11:06:48 -0700

    youtube video up to #9

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'youtube';
+  title = 'tutorial';
+  appName = 'youtube app'
+  name = "Maura"
+  getName(){
+    return `Hello ${this.name}`
+  }
+  obj={
+    name: 'Kelly',
+    age: 20
+  }
+  arr = ['avni', 'josh', 'jayant']
+  siteUrl=window.location.href
+  myName='Jayant'
+  getNameWithButton(newname: string) {
+    alert(newname);
+  }
+  myEvent(evt: string){
+    console.log(evt);
+  }
 }
